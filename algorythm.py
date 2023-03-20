abeb94681b31518d10a270c1ae43f9bdb16cc5fc
d.gurylev <d.gurylev@tdm-tech.ru>
2023-03-21 00:40:57 +0300

    upd [2]

--- algorythm.py
+++ algorythm.py
@@ -4,13 +4,13 @@ import node
 class Algorythm:
     def __init__(self):
         self.nodes = []
-        self.codes = {}
-        self.freq = {}
+        self.table_of_codes = {}
+        self.char_freq = {}
 
     def compress_str(self, source, debug):
         self.define_freq(source)
 
-        for k, v in self.freq.items():
+        for k, v in self.char_freq.items():
             self.nodes.append(node.Node(letter=k, freq=v))
         self.create_huffman_tree()
         self.create_table(self.nodes[0], '')
@@ -25,7 +25,7 @@ class Algorythm:
                 text += line
                 self.define_freq(line)
 
-        for k, v in self.freq.items():
+        for k, v in self.char_freq.items():
             self.nodes.append(node.Node(letter=k, freq=v))
         self.create_huffman_tree()
         table = {}
@@ -38,17 +38,17 @@ class Algorythm:
         #     f.write(decoded_text)
 
     def debug_info(self):
-        d = self.freq
-        t = self.codes
+        d = self.char_freq
+        t = self.table_of_codes
         print('letter\tfrequency\tcode')
         for k, v in d.items():
             print(k + '\t' + str(v) + '\t' + t[k])
 
-    def decode(encoded, table):
+    def decode(self, encoded):
         decoded = ''
         while len(encoded) > 0:
             found = False
-            for k, v in table.items():
+            for k, v in self.table_of_codes.items():
                 code_len = len(v)
                 if encoded[:code_len] == v:
                     decoded += k
@@ -60,20 +60,20 @@ class Algorythm:
         return decoded
 
     def encode(self, text):
-        return ''.join(self.codes[ch] for ch in text)
+        return ''.join(self.table_of_codes[ch] for ch in text)
 
-    def create_table(self, node, code):
-        if node.left is None and node.right is None:
-            self.codes[node.letter] = ''.join(str(x) for x in code)
+    def create_table(self, n, code):
+        if n.left is None and n.right is None:
+            self.table_of_codes[n.letter] = ''.join(str(x) for x in code)
         else:
-            self.create_table(node.left, code + '0')
-            self.create_table(node.right, code + '1')
+            self.create_table(n.left, code + '0')
+            self.create_table(n.right, code + '1')
 
     def create_huffman_tree(self):
         if len(self.nodes) == 1:
             return self.nodes
         else:
-            self.nodes.sort(key=lambda x: x.freq)
+            self.nodes.sort(key=lambda x: x.char_freq)
             parent = node.Node(left=self.nodes[0], right=self.nodes[1])
             self.nodes.append(parent)
             del self.nodes[0]
@@ -81,6 +81,6 @@ class Algorythm:
             self.create_huffman_tree()
 
     def define_freq(self, s: str):
-        d = self.freq
+        d = self.char_freq
         for ch in list(s):
             d[ch] = 1 if ch not in d.keys() else d[ch] + 1
