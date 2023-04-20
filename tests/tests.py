4d60f2c8ac36de2ab7e18454cf7dd1496cca3241
d.gurylev <d.gurylev@tdm-tech.ru>
2023-04-21 02:33:14 +0700

    upd tests [2]

--- tests/tests.py
+++ tests/tests.py
@@ -6,51 +6,54 @@ from unittest.mock import patch
 import main
 
 
+def get_args(original_file_name, compressed_file_name):
+    return [
+        "",
+        original_file_name,
+        compressed_file_name
+    ]
+
+
+def common(original_file_name, compressed_file_name, decompressed_file_name):
+    compress_args = get_args(original_file_name, compressed_file_name)
+    with unittest.mock.patch('sys.argv', compress_args):
+        main.main()
+
+    decompress_args = [
+        "",
+        "decode",
+        compressed_file_name,
+        decompressed_file_name
+    ]
+    with unittest.mock.patch('sys.argv', decompress_args):
+        main.main()
+    assert filecmp.cmp(original_file_name, decompressed_file_name)
+    os.remove(compressed_file_name)
+    os.remove(decompressed_file_name)
+
+
 class TestApp(unittest.TestCase):
-    def get_args(self, original_file_name, compressed_file_name):
-        return [
-            "",
-            original_file_name,
-            compressed_file_name
-        ]
 
     def test_1(self):
         original_file_name = 'test_files/test1.txt'
         compressed_file_name = 'tmp_files/test1_zip'
         decompressed_file_name = 'tmp_files/test1_res.txt'
-        self.common(original_file_name, compressed_file_name, decompressed_file_name)
+        common(original_file_name, compressed_file_name, decompressed_file_name)
 
     def test_2(self):
         original_file_name = 'test_files/test2.xml'
         compressed_file_name = 'tmp_files/test2_zip'
         decompressed_file_name = 'tmp_files/test2_res.xml'
-        self.common(original_file_name, compressed_file_name, decompressed_file_name)
+        common(original_file_name, compressed_file_name, decompressed_file_name)
 
     def test_3(self):
         original_file_name = 'test_files/test3.txt'
         compressed_file_name = 'tmp_files/test3_zip'
         decompressed_file_name = 'tmp_files/test3_res.txt'
-        self.common(original_file_name, compressed_file_name, decompressed_file_name)
+        common(original_file_name, compressed_file_name, decompressed_file_name)
 
     def test_4(self):
         original_file_name = 'test_files/test4.txt'
         compressed_file_name = 'tmp_files/test4_zip'
         decompressed_file_name = 'tmp_files/test4_res.txt'
-        self.common(original_file_name, compressed_file_name, decompressed_file_name)
-
-    def common(self, original_file_name, compressed_file_name, decompressed_file_name):
-        compress_args = self.get_args(original_file_name, compressed_file_name)
-        with unittest.mock.patch('sys.argv', compress_args):
-            main.main()
-
-        decompress_args = [
-            "",
-            "decode",
-            compressed_file_name,
-            decompressed_file_name
-        ]
-        with unittest.mock.patch('sys.argv', decompress_args):
-            main.main()
-        assert filecmp.cmp(original_file_name, decompressed_file_name)
-        os.remove(compressed_file_name)
-        os.remove(decompressed_file_name)
+        common(original_file_name, compressed_file_name, decompressed_file_name)
