d2909045b254eee008ff16d90e572804a99f89b7
d.gurylev <d.gurylev@tdm-tech.ru>
2023-04-20 20:13:34 +0700

    minor fix: rename [4]

--- tests/tests.py
+++ tests/tests.py
@@ -35,8 +35,9 @@ class TestApp(unittest.TestCase):
 
     def test_2(self):
         original_file_name = 'test_files/test2.xml'
-
         compressed_file_name = 'tmp_files/test2_zip'
+        decompressed_file_name = 'tmp_files/test2_res.xml'
+
         compress_args = [
             "",
             original_file_name,
@@ -45,7 +46,6 @@ class TestApp(unittest.TestCase):
         with unittest.mock.patch('sys.argv', compress_args):
             main.main()
 
-        decompressed_file_name = 'tmp_files/test2_res.xml'
         decompress_args = [
             "",
             "decode",
@@ -60,8 +60,9 @@ class TestApp(unittest.TestCase):
 
     def test_3(self):
         original_file_name = 'test_files/test3.txt'
-
         compressed_file_name = 'tmp_files/test3_zip'
+        decompressed_file_name = 'tmp_files/test3_res.txt'
+
         compress_args = [
             "",
             original_file_name,
@@ -70,7 +71,31 @@ class TestApp(unittest.TestCase):
         with unittest.mock.patch('sys.argv', compress_args):
             main.main()
 
-        decompressed_file_name = 'tmp_files/test3_res.txt'
+        decompress_args = [
+            "",
+            "decode",
+            compressed_file_name,
+            decompressed_file_name
+        ]
+        with unittest.mock.patch('sys.argv', decompress_args):
+            main.main()
+        assert filecmp.cmp(original_file_name, decompressed_file_name)
+        os.remove(compressed_file_name)
+        os.remove(decompressed_file_name)
+
+    def test_4(self):
+        original_file_name = 'test_files/test4.txt'
+        compressed_file_name = 'tmp_files/test4_zip'
+        decompressed_file_name = 'tmp_files/test4_res.txt'
+
+        compress_args = [
+            "",
+            original_file_name,
+            compressed_file_name
+        ]
+        with unittest.mock.patch('sys.argv', compress_args):
+            main.main()
+
         decompress_args = [
             "",
             "decode",
