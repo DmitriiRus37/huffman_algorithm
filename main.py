a2cdbbfd11d053d65af7cdc6977de7b5564731d9
d.gurylev <d.gurylev@tdm-tech.ru>
2023-05-17 17:35:59 +1000

    added TODO

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from decompression import Decompression
 from helpers import get_compression_info
 
 
-# TODO: make encoding and decoding with pieces of files
-
+# TODO add compressing of some files to one compressed_file
 def main():
     if sys.argv[1] == 'decode':
         source_path = os.path.abspath(sys.argv[2])
